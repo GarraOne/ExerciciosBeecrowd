f570af7b63eaf4c4474ed4889e7afb96f19b0d63
Gabriel Teotonio Cordeiro <164008767+GarraOne@users.noreply.github.com>
2024-05-20 08:09:20 -0300

    Create 1021.java

---
+++ Iniciante/1021 Notas e Moedas/1021.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+import java.io.IOException;
+
+
+public class Main {
+
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        
+        
+        double A = sc.nextDouble();
+        int X = (int)A;
+
+        int A1 = X/100;
+        int A2 =(X - A1*100)/50;
+        int A3 =(X -(A1*100+A2*50) )/20;
+        int A4 =(X -(A1*100+A2*50+A3*20) )/10;
+        int A5 =(X -(A1*100+A2*50+A3*20+A4*10) )/5;
+        int A6 =(X -(A1*100+A2*50+A3*20+A4*10+A5*5) )/2;
+        int A7 =(X -(A1*100+A2*50+A3*20+A4*10+A5*5+A6*2) )/1;
+        
+        double XXX  = (A - X)*100;
+        int XX = (int)XXX;
+        
+        int AA1 =(XX/100);
+        int AA2 =(XX -(AA1*100) )/50;
+        int AA3 =(XX -(AA1*100+AA2*50) )/25;
+        int AA4 =(XX -(AA1*100+AA2*50+AA3*25) )/10;
+        int AA5 =(XX -(AA1*100+AA2*50+AA3*25+AA4*10) )/5;
+        int AA6 =(XX -(AA1*100+AA2*50+AA3*25+AA4*10+AA5*5) )/1;
+
+        System.out.println("NOTAS:" );
+
+        System.out.println(A1+" nota(s) de R$ 100.00" );
+        System.out.println(A2+" nota(s) de R$ 50.00" );
+        System.out.println(A3+" nota(s) de R$ 20.00" );
+        System.out.println(A4+" nota(s) de R$ 10.00" );
+        System.out.println(A5+" nota(s) de R$ 5.00" );
+        System.out.println(A6+" nota(s) de R$ 2.00" );
+
+        System.out.println("MOEDAS:" );
+
+        System.out.println(A7+" moeda(s) de R$ 1.00" );
+        System.out.println(AA2+" moeda(s) de R$ 0.50" );
+        System.out.println(AA3+" moeda(s) de R$ 0.25" );
+        System.out.println(AA4+" moeda(s) de R$ 0.10" );
+        System.out.println(AA5+" moeda(s) de R$ 0.05" );
+        System.out.println(AA6+" moeda(s) de R$ 0.01" );
+      
+}
+       
+}
+   
