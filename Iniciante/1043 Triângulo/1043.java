41179e8575519df40d1e6000bf3a5d852c5f1b74
Gabriel Teotonio Cordeiro <164008767+GarraOne@users.noreply.github.com>
2024-05-24 19:13:41 -0300

    Update 1043.java

--- Iniciante/1043 Triângulo/1043.java
+++ Iniciante/1043 Triângulo/1043.java
@@ -1,10 +1,9 @@
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
  
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)  {
         Scanner sc = new Scanner(System.in);
         double A = sc.nextDouble();      
         double B = sc.nextDouble();      
