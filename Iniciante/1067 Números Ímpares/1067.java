0f86ac3d5f1daf03df180e1ae1ffc299f53c294a
Gabriel Teotônio Cordeiro <164008767+GarraOne@users.noreply.github.com>
2024-05-28 07:55:53 -0300

    Create 1067.java

---
+++ Iniciante/1067 Números Ímpares/1067.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+public class Main {
+
+ 
+    public static void main(String[] args)  {
+        Scanner sc = new Scanner(System.in);
+double X = sc.nextDouble();      
+
+int Y =2;
+
+for( Y = 1; Y <= X  ; Y++ )
+
+{ 
+   if (Y % 2 !=	0 )System.out.println(Y); 
+     }
+  }
+}
