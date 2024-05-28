9007c55cf155119b91f2220154a9432e23e8149b
Gabriel Teotônio Cordeiro <164008767+GarraOne@users.noreply.github.com>
2024-05-28 10:19:45 -0300

    Create 1075.java

---
+++ Iniciante/1075 Resto 2/1075.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args)  {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();      
+        int Y = 0;
+        String I = "";
+        String II = "";
+for( Y = 1; Y <= 10000   ; Y++ )
+{
+    if ( Y % N == 2 )    
+    {
+    System.out.println(Y);
+    }
+        }
+    }
+}
