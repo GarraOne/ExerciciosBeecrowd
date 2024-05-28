7d7e2b5bece329a708a5787d963092466f1db60b
Gabriel Teotônio Cordeiro <164008767+GarraOne@users.noreply.github.com>
2024-05-28 09:36:53 -0300

    Create 1026.java

---
+++ AD-HOC/1026 Carrega ou não Carrega?/1026.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()){
+        long X = sc.nextLong();
+        long Y = sc.nextLong();
+        long N = X ^ Y;
+        
+        System.out.println(N);
+        }
+    }
+}
