81473a9184976b80bdcbe04eec469dae27d00846
Gabriel Teotônio Cordeiro <164008767+GarraOne@users.noreply.github.com>
2024-05-28 10:26:00 -0300

    Create 1080.java

---
+++ Iniciante/1080 Maior e Posição/1080.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int Y = 0;
+        int I = 0;
+        int P = 0;
+        for( Y = 1; Y <= 100   ; Y++ )
+        {
+            
+    int N = sc.nextInt();      
+    if (  N > I )  
+   { 
+       I = N; P = Y;
+      
+   }
+}
+    System.out.println(I);
+    System.out.println(P);
+        
+    }
+
+}
