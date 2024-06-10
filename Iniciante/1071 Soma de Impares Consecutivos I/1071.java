159a14b441e4f4412aabe15f7e0678d7ac80a568
Gabriel Teotônio Cordeiro <164008767+GarraOne@users.noreply.github.com>
2024-06-10 10:14:00 -0300

    Update 1071.java

--- Iniciante/1071 Soma de Impares Consecutivos I/1071.java
+++ Iniciante/1071 Soma de Impares Consecutivos I/1071.java
@@ -3,19 +3,15 @@ public class Main {
     public static void main(String[] args)  {
     	Scanner sc = new Scanner(System.in);
 		int XX = sc.nextInt(), YY = sc.nextInt(), S = 0, I = 0, X, Y;
-		
 		if (XX < YY){ X = XX; Y = YY;}
 		else { X = YY; Y = XX; }
-		
 		I = X;
 		if(I%2 == 0) {I++;}
+		else {S = S - I;}
 		
 		while(I < Y)
 		{
-		    if (I != X)
-		    {
 		        S = S + I;
-		    }
 		        I = I + 2;
 		}
 		System.out.println(S);
