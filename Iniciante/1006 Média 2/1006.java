73bcdb21b1804d56ebc22397cb17077f9741f728
Gabriel Teotônio Cordeiro <164008767+GarraOne@users.noreply.github.com>
2024-06-20 01:55:32 -0300

    Update 1006.java

--- Iniciante/1006 Média 2/1006.java
+++ Iniciante/1006 Média 2/1006.java
@@ -1,20 +1,10 @@
-import java.io.IOException;
 import java.util.Scanner;
-
 public class Main {
-
- 
-    public static void main(String[] args) throws IOException {
-        
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double A = sc.nextDouble();
-        double B = sc.nextDouble();
-        double C = sc.nextDouble();
-        double AA = A*2 ;
-        double BB = B*3 ;
-        double CC = C*5 ;
-
-        double X = (AA+BB+CC)/10 ;
+        double A = sc.nextDouble(), B = sc.nextDouble(), C = sc.nextDouble(),
+        AA = A*2, BB = B*3, CC = C*5,
+        X = (AA+BB+CC)/10;
         System.out.println(String.format("MEDIA = %.1f", X));
     }
 }
