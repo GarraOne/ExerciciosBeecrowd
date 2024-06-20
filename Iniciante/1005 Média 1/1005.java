6a9c9de35ce7271932c3cc133dc7005050737b10
Gabriel Teotônio Cordeiro <164008767+GarraOne@users.noreply.github.com>
2024-06-20 01:53:50 -0300

    Update 1005.java

--- Iniciante/1005 Média 1/1005.java
+++ Iniciante/1005 Média 1/1005.java
@@ -1,17 +1,9 @@
-import java.io.IOException;
 import java.util.Scanner;
-
 public class Main {
-
- 
-    public static void main(String[] args) throws IOException {
-        
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double A = sc.nextDouble();
-        double B = sc.nextDouble();
-        double AA = A*3.5 ;
-        double BB = B*7.5 ;
-        double X = (AA+BB)/11 ;
-        System.out.println(String.format("MEDIA = %.5f", X));   
+        double A = sc.nextDouble(), B = sc.nextDouble(),
+        AA = A*3.5 , BB = B*7.5, X = (AA+BB)/11 ;
+        System.out.println(String.format("MEDIA = %.5f", X));
     }
 }
