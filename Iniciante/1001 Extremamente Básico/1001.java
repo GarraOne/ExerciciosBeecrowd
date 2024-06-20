643d0661f771a0c1f21dfd9db9fdbe3e153dede1
Gabriel Teotônio Cordeiro <164008767+GarraOne@users.noreply.github.com>
2024-06-20 01:46:58 -0300

    Update 1001.java

--- Iniciante/1001 Extremamente Básico/1001.java
+++ Iniciante/1001 Extremamente Básico/1001.java
@@ -1,15 +1,9 @@
-import java.io.IOException;
 import java.util.Scanner;
-
 public class Main {
-
- 
-    public static void main(String[] args) throws IOException {
-        
+    public static void main(String[] args)  {
         Scanner sc = new Scanner(System.in);
-        int A = sc.nextInt();
-        int B = sc.nextInt();
-        int X = A+B;
-        System.out.println("X = " + X);
+        int A = sc.nextInt(), B = sc.nextInt(); 
+        A = A+B;
+        System.out.println("X = " + A);
     }
 }
