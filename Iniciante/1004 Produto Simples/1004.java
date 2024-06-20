a82596b86b042a19d7167685f4fb986332963de3
Gabriel Teotônio Cordeiro <164008767+GarraOne@users.noreply.github.com>
2024-06-20 01:51:47 -0300

    Update 1004.java

--- Iniciante/1004 Produto Simples/1004.java
+++ Iniciante/1004 Produto Simples/1004.java
@@ -1,14 +1,8 @@
-import java.io.IOException;
 import java.util.Scanner;
-
 public class Main {
-
-    public static void main(String[] args) throws IOException {
-        
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int A = sc.nextInt();
-        int B = sc.nextInt();
-        int X = A*B;
-        System.out.println("PROD = " + X);   
+        int X = sc.nextInt() * sc.nextInt();
+        System.out.println("PROD = " + X);
     }
 }
