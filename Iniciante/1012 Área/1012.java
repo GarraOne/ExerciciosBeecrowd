fdfdd16521c655fb0065258dc3b182ca32723ee7
Gabriel Teotônio Cordeiro <164008767+GarraOne@users.noreply.github.com>
2024-06-20 02:05:41 -0300

    Update 1012.java

--- Iniciante/1012 Área/1012.java
+++ Iniciante/1012 Área/1012.java
@@ -1,35 +1,16 @@
-import java.io.IOException;
 import java.util.Scanner;
-
 public class Main {
-
- 
-    public static void main(String[] args) throws IOException {
-   
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        double A = sc.nextDouble();
-        double B = sc.nextDouble();
-        double C = sc.nextDouble();
-        
-        double T = (A*C)/2 ;
-        
-        double CC = C*C*3.14159 ;
-        
-        double TT =((A+B)*C)/2;
-        
-        double Q = B*B;
-        
-        double R = A*B;
-        
-        
+        double A = sc.nextDouble(), B = sc.nextDouble(), C = sc.nextDouble(),
+        T = (A*C)/2,
+        CC = C*C*3.14159,
+        TT =((A+B)*C)/2,
+        Q = B*B, R = A*B;
         System.out.println(String.format("TRIANGULO: %.3f", T ));
         System.out.println(String.format("CIRCULO: %.3f", CC ));
         System.out.println(String.format("TRAPEZIO: %.3f", TT ));
         System.out.println(String.format("QUADRADO: %.3f", Q ));
         System.out.println(String.format("RETANGULO: %.3f", R ));
-        
-        
     }
-    
 }
