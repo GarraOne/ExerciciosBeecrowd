2c7a419502a069cbf0a44b2458e699461d8331df
Gabriel Teotônio Cordeiro <164008767+GarraOne@users.noreply.github.com>
2024-06-20 02:03:49 -0300

    Update 1011.java

--- Iniciante/1011 Esfera/1011.java
+++ Iniciante/1011 Esfera/1011.java
@@ -1,14 +1,9 @@
-import java.io.IOException;
 import java.util.Scanner;
-
 public class Main {
-
- 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double A = sc.nextDouble();
-        double X = (4.0/3)*3.14159*A*A*A;
+        double A = sc.nextDouble(),
+        X = (4.0/3)*3.14159*A*A*A;
         System.out.println(String.format("VOLUME = %.3f", X));
     }
-    
 }
