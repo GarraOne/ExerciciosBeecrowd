344d07d4e37d678d841fb8759fc8313c1ed16e2a
Gabriel Teotônio Cordeiro <164008767+GarraOne@users.noreply.github.com>
2024-06-20 02:02:38 -0300

    Update 1010.java

--- Iniciante/1010 Cálculo Simples/1010.java
+++ Iniciante/1010 Cálculo Simples/1010.java
@@ -1,21 +1,12 @@
-import java.io.IOException;
 import java.util.Scanner;
-
 public class Main {
-
- 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double A = sc.nextDouble();
-        double B = sc.nextDouble();
-        double C = sc.nextDouble();
-        double X = B*C ;
-        double AA = sc.nextDouble();
-        double BB = sc.nextDouble();
-        double CC = sc.nextDouble();
-        double V = BB*CC ;
-        double T = V + X ;
+        double A = sc.nextDouble(), B = sc.nextDouble(), C = sc.nextDouble(),
+        X = B*C ,
+        AA = sc.nextDouble(), BB = sc.nextDouble(), CC = sc.nextDouble(),
+        V = BB*CC,
+        T = V + X;
         System.out.println(String.format("VALOR A PAGAR: R$ %.2f", T));
     }
-    
 }
