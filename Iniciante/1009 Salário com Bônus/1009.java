dbbeaefec5277210110fa3381cd872795e330f3e
Gabriel Teotônio Cordeiro <164008767+GarraOne@users.noreply.github.com>
2024-06-20 02:00:02 -0300

    Update 1009.java

--- Iniciante/1009 Salário com Bônus/1009.java
+++ Iniciante/1009 Salário com Bônus/1009.java
@@ -1,15 +1,10 @@
-import java.io.IOException;
 import java.util.Scanner;
-
 public class Main {
-
- 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String nome = sc.nextLine();
-        double A = sc.nextDouble();
-        double B = sc.nextDouble();
-        double X = A+(B*0.15) ;
+        double A = sc.nextDouble(), B = sc.nextDouble(),
+        X = A+(B*0.15) ;
         System.out.println(String.format("TOTAL = R$ %.2f", X));
     }
 }
