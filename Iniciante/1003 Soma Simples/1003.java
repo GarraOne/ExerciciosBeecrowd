97ce6a11117180ac9d1c6c26439c7266f7d78269
Gabriel Teotônio Cordeiro <164008767+GarraOne@users.noreply.github.com>
2024-06-20 01:49:42 -0300

    Update 1003.java

--- Iniciante/1003 Soma Simples/1003.java
+++ Iniciante/1003 Soma Simples/1003.java
@@ -1,16 +1,8 @@
-import java.io.IOException;
 import java.util.Scanner;
-
 public class Main {
-
- 
-    public static void main(String[] args) throws IOException {
-        
+    public static void main(String[] args)  {
         Scanner sc = new Scanner(System.in);
-        int A = sc.nextInt();
-        int B = sc.nextInt();
-        int X = A+B;
+        int A = sc.nextInt(), B = sc.nextInt(), X = A+B;
         System.out.println("SOMA = " + X);
-
     }
 }
