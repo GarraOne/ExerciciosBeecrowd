bd9fcac6cb01b4e46f0c0904e622a75397e85050
Gabriel Teotônio Cordeiro <164008767+GarraOne@users.noreply.github.com>
2024-06-23 19:24:52 -0300

    Update 1013.java

--- Iniciante/1013 O Maior/1013.java
+++ Iniciante/1013 O Maior/1013.java
@@ -1,18 +1,10 @@
-import java.io.IOException;
 import java.util.Scanner;
-
 public class Main {
-
- 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int A = sc.nextInt();
-        int B = sc.nextInt();
-        int C = sc.nextInt();
-        
-        int AB = (A + B + Math.abs(A - B))/2 ;
-        int X = (AB + C + Math.abs(AB - C))/2 ;
+        int A = sc.nextInt(), B = sc.nextInt(), C = sc.nextInt(),
+        AB = (A + B + Math.abs(A - B))/2,
+        X = (AB + C + Math.abs(AB - C))/2 ;
         System.out.println(  X + " eh o maior" );
     }
-    
 }
