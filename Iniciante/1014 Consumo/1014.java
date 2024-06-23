218ec291a5fde7a324cdbaf2b3745c7dbd3367e1
Gabriel Teotônio Cordeiro <164008767+GarraOne@users.noreply.github.com>
2024-06-23 20:08:03 -0300

    Update 1014.java

--- Iniciante/1014 Consumo/1014.java
+++ Iniciante/1014 Consumo/1014.java
@@ -1,15 +1,8 @@
-import java.io.IOException;
 import java.util.Scanner;
-
 public class Main {
-
- 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)  {
         Scanner sc = new Scanner(System.in);
-        double A = sc.nextDouble();
-        double B = sc.nextDouble();
-        double X = A/B ;
+        double A = sc.nextDouble(), B = sc.nextDouble(), X = A/B;
         System.out.println(String.format("%.3f km/l",X ));
     }
-    
 }
