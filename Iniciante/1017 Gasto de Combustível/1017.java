e52f4f51e489af718de50cac170cd230de40f694
Gabriel Teotônio Cordeiro <164008767+GarraOne@users.noreply.github.com>
2024-06-23 20:44:48 -0300

    Update 1017.java

--- Iniciante/1017 Gasto de Combustível/1017.java
+++ Iniciante/1017 Gasto de Combustível/1017.java
@@ -1,15 +1,9 @@
-import java.io.IOException;
 import java.util.Scanner;
-
 public class Main {
-
- 
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
-        double X = sc.nextDouble();
-        double Y = sc.nextDouble();
-        
-        double D = X*Y/12;
+        double X = sc.nextDouble(), Y = sc.nextDouble(),
+        D = X*Y/12;
         System.out.println(String.format("%.3f",D ));
     }
 }
