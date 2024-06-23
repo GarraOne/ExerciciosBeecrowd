5e6b1f7a54dd28c629a778489ec18b00ed9e5c79
Gabriel Teotônio Cordeiro <164008767+GarraOne@users.noreply.github.com>
2024-06-23 20:01:27 -0300

    Update 1015.java

--- Iniciante/1015 Distância Entre Dois Pontos/1015.java
+++ Iniciante/1015 Distância Entre Dois Pontos/1015.java
@@ -1,17 +1,10 @@
-import java.io.IOException;
 import java.util.Scanner;
-
 public class Main {
-
- 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)  {
         Scanner sc = new Scanner(System.in);
-        double X = sc.nextDouble();
-        double Y = sc.nextDouble();
-        double XX = sc.nextDouble();
-        double YY = sc.nextDouble();
-        double D = Math.sqrt((Math.pow((XX-X),2)) + (Math.pow((YY-Y), 2)))  ;
+        double X = sc.nextDouble(), Y = sc.nextDouble(),
+        XX = sc.nextDouble(), YY = sc.nextDouble(),
+        D = Math.sqrt((Math.pow((XX-X),2)) + (Math.pow((YY-Y), 2)))  ;
         System.out.println(String.format("%.4f",D ));
     }
-    
 }
