f846ec276f1751cd944767935d52303cd1176804
Gabriel Teotônio Cordeiro <164008767+GarraOne@users.noreply.github.com>
2024-06-23 20:48:23 -0300

    Update 1018.java

--- Iniciante/1018 Cédulas/1018.java
+++ Iniciante/1018 Cédulas/1018.java
@@ -1,20 +1,16 @@
-import java.io.IOException;
 import java.util.Scanner;
-
 public class Main {
-
- 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int X = sc.nextInt();
         
-        int A1 = X/100;
-        int A2 =(X - A1*100)/50;
-        int A3 =(X -(A1*100+A2*50) )/20;
-        int A4 =(X -(A1*100+A2*50+A3*20) )/10;
-        int A5 =(X -(A1*100+A2*50+A3*20+A4*10) )/5;
-        int A6 =(X -(A1*100+A2*50+A3*20+A4*10+A5*5) )/2;
-        int A7 =(X -(A1*100+A2*50+A3*20+A4*10+A5*5+A6*2) )/1;
+        int A1 = X/100,
+        A2 =(X - A1*100)/50,
+        A3 =(X -(A1*100+A2*50) )/20,
+        A4 =(X -(A1*100+A2*50+A3*20) )/10,
+        A5 =(X -(A1*100+A2*50+A3*20+A4*10) )/5,
+        A6 =(X -(A1*100+A2*50+A3*20+A4*10+A5*5) )/2,
+        A7 =(X -(A1*100+A2*50+A3*20+A4*10+A5*5+A6*2) )/1;
 
         System.out.println(X);
         System.out.println(A1+" nota(s) de R$ 100,00" );
@@ -24,8 +20,5 @@ public class Main {
         System.out.println(A5+" nota(s) de R$ 5,00" );
         System.out.println(A6+" nota(s) de R$ 2,00" );
         System.out.println(A7+" nota(s) de R$ 1,00" );
-
-        
-        
     }
 }
