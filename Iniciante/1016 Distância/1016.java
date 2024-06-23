48ea8306d3cea0b5aacfaff3aa16610d8042f9b4
Gabriel Teotônio Cordeiro <164008767+GarraOne@users.noreply.github.com>
2024-06-23 20:31:17 -0300

    Update 1016.java

--- Iniciante/1016 Distância/1016.java
+++ Iniciante/1016 Distância/1016.java
@@ -1,13 +1,8 @@
-import java.io.IOException;
 import java.util.Scanner;
-
 public class Main {
-
- 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)  {
         Scanner sc = new Scanner(System.in);
-        int A = sc.nextInt();
-        int X = A*2;
+        int A = sc.nextInt(), X = A*2;
         System.out.println(X + " minutos");
     }
 }
