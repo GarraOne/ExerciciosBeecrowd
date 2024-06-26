1fa3e5c2b9c7a48d9e8511a317b408adac6dbe01
Gabriel Teotônio Cordeiro <164008767+GarraOne@users.noreply.github.com>
2024-06-25 22:14:14 -0300

    Update 1019.java

--- Iniciante/1019 Conversão de Tempo/1019.java
+++ Iniciante/1019 Conversão de Tempo/1019.java
@@ -2,8 +2,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args)  {
         Scanner sc = new Scanner(System.in);
-        int X = sc.nextInt(), A = X/3600 ,
-        B = (X - (A*3600))/60, C = (X -( A*3600 + B*60));
+        int X = sc.nextInt(), 
+        A = X/3600 ,
+        B = (X - (A*3600))/60, 
+        C = (X -( A*3600 + B*60));
         System.out.println(A +":"+ B +":"+ C);
     }
 }
