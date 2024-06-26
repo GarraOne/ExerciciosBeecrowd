03d2b7b0f2e390524a9074957759bb5688fa7498
Gabriel Teotônio Cordeiro <164008767+GarraOne@users.noreply.github.com>
2024-06-25 22:09:40 -0300

    Update 1019.java

--- Iniciante/1019 Conversão de Tempo/1019.java
+++ Iniciante/1019 Conversão de Tempo/1019.java
@@ -1,20 +1,9 @@
-import java.io.IOException;
 import java.util.Scanner;
-
 public class Main {
-
- 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)  {
         Scanner sc = new Scanner(System.in);
-        int X = sc.nextInt();
-        
-        int A = X/3600 ;
-        int B = (X - (A*3600))/60 ;
-        int C = (X -( A*3600 + B*60));
-
+        int X = sc.nextInt(), A = X/3600 ,
+        B = (X - (A*3600))/60, C = (X -( A*3600 + B*60));
         System.out.println(A +":"+ B +":"+ C);
-
-        
-        
     }
 }
